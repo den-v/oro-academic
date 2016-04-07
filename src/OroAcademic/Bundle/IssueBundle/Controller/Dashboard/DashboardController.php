113f196ebe341e9a48e0fedc6641a2691bf27908
Volodymyr Denchyk <devit_dvv@ukr.net>
2016-04-07 11:56:54 +0300

    OOT-1158: Grid and Chart Widgets

---
+++ src/OroAcademic/Bundle/IssueBundle/Controller/Dashboard/DashboardController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace OroAcademic\Bundle\IssueBundle\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route(
+     *      "/issues_by_status/chart/{widget}",
+     *      name="oroacademic_dashboard_issues_by_state_chart",
+     *      requirements={"widget"="[\w-]+"}
+     * )
+     * @Template("OroAcademicIssueBundle:Dashboard:issuesByStatus.html.twig")
+     */
+    public function issuesByStatusAction($widget)
+    {
+        $items = $this->getDoctrine()
+            ->getRepository('OroAcademicIssueBundle:Issue')
+            ->getIssuesByStatus(
+                $this->get('oro_security.acl_helper'),
+                $this->get('oro_dashboard.widget_configs')
+                    ->getWidgetOptions($this->getRequest()->query->get('_widgetId', null))
+                    ->get('dateRange')
+            );
+
+        $widgetAttr = $this->get('oro_dashboard.widget_configs')->getWidgetAttributesForTwig($widget);
+        $widgetAttr['chartView'] = $this->get('oro_chart.view_builder')
+            ->setArrayData($items)
+            ->setOptions(
+                array(
+                    'name' => 'bar_chart',
+                    'data_schema' => array(
+                        'label' => array('field_name' => 'label'),
+                        'value' => array(
+                            'field_name' => 'issues'
+                        )
+                    ),
+                    'settings' => array('xNoTicks' => 2),
+                )
+            )
+            ->getView();
+
+        return $widgetAttr;
+    }
+
+    /**
+     * @Route("/issues_widget_grid/{widget}", name="oroacademic_dashboard_issues_widget_grid", requirements={"widget"="[\w-]+"})
+     * @Template("OroAcademicIssueBundle:Dashboard:widgetIssues.html.twig")
+     *
+     * @param $widget
+     * @return array $widgetAttr
+     */
+    public function issueShortGridAction($widget)
+    {
+        $widgetAttr = $this->get('oro_dashboard.widget_configs')->getWidgetAttributesForTwig($widget);
+        $widgetAttr['entity'] = $this->getUser();
+        return $widgetAttr;
+    }
+}
--- src/OroAcademic/Bundle/IssueBundle/Entity/Repository/IssueRepository.php
+++ src/OroAcademic/Bundle/IssueBundle/Entity/Repository/IssueRepository.php
@@ -5,9 +5,75 @@ namespace OroAcademic\Bundle\IssueBundle\Entity\Repository;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 
+use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 use OroAcademic\Bundle\IssueBundle\Entity\Issue;
 
 class IssueRepository extends EntityRepository
 {
+    /**
+     * Get opportunities by state by current quarter
+     *
+     * @param $aclHelper AclHelper
+     * @param  array     $dateRange
+     * @return array
+     */
+    public function getIssuesByStatus(AclHelper $aclHelper, $dateRange)
+    {
+        $dateEnd = $dateRange['end'];
+        $dateStart = $dateRange['start'];
+
+        return $this->getIssuesDataByStatus($aclHelper, $dateStart, $dateEnd);
+    }
+
+    /**
+     * @param  AclHelper $aclHelper
+     * @param $dateStart
+     * @param $dateEnd
+     * @return array
+     */
+    protected function getIssuesDataByStatus(AclHelper $aclHelper, $dateStart = null, $dateEnd = null)
+    {
+        // select all possible statuses
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('status.name, status.label')
+            ->from('OroWorkflowBundle:WorkflowStep', 'status')
+            ->where('status.definition = \'oroacademic_issue_flow\'')
+            ->orderBy('status.name', 'ASC');
+
+        $resultData = array();
+        $data = $qb->getQuery()->getArrayResult();
+        foreach ($data as $status) {
+            $name = $status['name'];
+            $label = $status['label'];
+            $resultData[$name] = array(
+                'name' => $name,
+                'label' => $label,
+                'issues' => 0,
+            );
+        }
+
+        // select issues data
+        $qb = $this->createQueryBuilder('issue');
+        $qb->select('COUNT(issue.id) as issues, step.name as status')
+            ->leftJoin('issue.workflowStep', 'step')
+            ->groupBy('step.label');
+
+        if ($dateStart && $dateEnd) {
+            $qb->where($qb->expr()->between('issue.createdAt', ':dateFrom', ':dateTo'))
+                ->setParameter('dateFrom', $dateStart)
+                ->setParameter('dateTo', $dateEnd);
+        }
+        $groupedData = $aclHelper->apply($qb)->getArrayResult();
+
+        foreach ($groupedData as $statusData) {
+            $status = $statusData['status'];
+            $issues = (int)$statusData['issues'];
+            if ($issues) {
+                $resultData[$status]['issues'] = $issues;
+            }
+        }
+
+        return $resultData;
+    }
 
 }
